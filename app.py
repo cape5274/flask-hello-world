e39039890396bf97043722964455190066e9b8a2
Carolina Amber Perez <cape5274@colorado.edu>
2026-04-07 22:00:39 -0600

    Update hello_world function response message

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask
-import psycopg2
-
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello World from YOUR NAME in 3308'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
