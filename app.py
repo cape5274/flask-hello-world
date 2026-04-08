a4054b1e1e777eba3d5be0aac4121bb3ce2b3feb
Carolina Amber Perez <cape5274@colorado.edu>
2026-04-07 22:37:44 -0600

    Add database connection test route

--- app.py
+++ app.py
@@ -4,6 +4,13 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World from YOUR NAME in 3308'
+    
+@app.route("/db_test")
+def db_test():
+    conn = psycopg2.connect("postgresql://lab10_database_u7eo_user:JYUDlSCAXdtr5RFEbJ9XpF3dmIIvBmEI@dpg-d7atjjk50q8c73as43g0-a/lab10_database_u7eo")
+    conn.close
+    
+    return "Database connection works!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
