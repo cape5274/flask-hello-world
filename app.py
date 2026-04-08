e14ecc21efe89cff1b94503b99143e94e364de1d
Carolina Amber Perez <cape5274@colorado.edu>
2026-04-07 23:19:45 -0600

    Add route to create Basketball table in database

--- app.py
+++ app.py
@@ -13,5 +13,22 @@ def db_test():
     
     return "Database connection works!"
 
+@app.route("/db_create")
+def creating():
+    conn = psycopg2.connect("postgresql://lab10_database_u7eo_user:JYUDlSCAXdtr5RFEbJ9XpF3dmIIvBmEI@dpg-d7atjjk50q8c73as43g0-a/lab10_database_u7eo")
+    cur = conn.cursor()
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS Basketball (
+    First varchar(255),
+    Last varchar(255),
+    City varchar(255),
+    Name varchar(255),
+    Number int
+    );
+    ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Created"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
